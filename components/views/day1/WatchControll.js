183206a424a9fae837346d4ff50706a135f33205
matias <matiaskochman@gmail.com>
2016-12-31 15:47:56 -0300

    day1Excercise, working in watchcontroll 1

--- components/views/day1/WatchControll.js
+++ components/views/day1/WatchControll.js
@@ -1,12 +1,37 @@
 import React , { Component } from 'react';
 import { Platform,ListView,StyleSheet,StatusBar,Text,TouchableHighlight,View } from 'react-native';
+import Util from '../../Utils';
 
 class WatchControll extends Component{
   render(){
     return(
-      <View>
-        <Text>WatchControll
-        </Text>
+      <View style={{width: Util.size.width,
+        height: 110,
+        flexDirection:"row",
+        backgroundColor: '#f3f3f3',
+        paddingTop: 20, paddingLeft: 60, paddingRight:60, paddingBottom:0,
+      }}>
+        <View style={{flex:1,alignItems:"flex-start"}}>
+          <TouchableHighlight style={{width: 70,height: 70,borderRadius: 35,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}}>
+            <Text style={{fontSize:14,backgroundColor:"transparent",color:"#555"}}>Stop</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={{flex:1,alignItems:"flex-end"}}>
+          <TouchableHighlight style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor:"#fff",
+            alignItems:"center",
+            justifyContent:"center"
+          }}>
+            <Text style={{
+              fontSize:14,
+              backgroundColor:"transparent"
+            }}>Start
+            </Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
